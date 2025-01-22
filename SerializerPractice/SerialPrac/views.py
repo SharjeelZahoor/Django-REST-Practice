f2c3183e242607c442b850e16af819b69abfb3e6
SharjeelZahoor <sherrysoftsol@gmail.com>
2025-01-22 10:12:29 +0500

    Updated project files

--- SerializerPractice/SerialPrac/views.py
+++ SerializerPractice/SerialPrac/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse 
 from rest_framework.renderers import JSONRenderer
 # Create your views here.
 
@@ -11,9 +11,16 @@ def student_detailone(request, pk):
     serializer = StudentSerializer(stu)
     #print(serializer)
     #print(serializer.data)
-    json_data = JSONRenderer().render(serializer.data)
+    #######
+    ###############json_data = JSONRenderer().render(serializer.data)
+    #######
     #print(json_data)
-    return(HttpResponse(json_data, content_type = 'application/json'))
+    #######we may use just one line which is at the last in place of these two lines
+    ###############return(HttpResponse(json_data, content_type = 'application/json'))
+    #######
+    return JsonResponse(serializer.data)
+    # if we have serializer.data which is not dict object then we have to write
+    # return JsonResponse(serializer.data, safe = 'False')
 
 def student_details(request):
     stu = Student.objects.all()
